d109f3499bc417e5bcae146ee511dee6be4b710f
Usuario <yishaqr2020@gmail.com>
2026-05-14 21:37:23 -0500

    feat: creacion de trabjao en clases api-usuario

---
+++ api-usuarios/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.dsi3"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web") 
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.xerial:sqlite-jdbc:3.45.1.0") 
+    implementation("org.hibernate.community:hibernate-community-dialects:6.4.4.Final")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ api-usuarios/src/test/java/com/dsi3/api_usuarios/ApiUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsi3.api_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
